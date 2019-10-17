6df1b1e1ca49138703073be237e1fdeac862dd8f
Syrus <me@syrusakbary.com>
2019-10-16 18:37:56 -0700

    Improved package metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup
+import os
+
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+
+def get_long_description():
+    readme_md = os.path.join(CURRENT_DIR, "README.md")
+    with open(readme_md) as ld_file:
+        return ld_file.read()
+
 
 setup(
     name='wasic',
     description="Wasic is a C/C++ Compiler toolchain for WASI",
-    version='0.1.4',
+    version='0.1.5',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    keywords="webassembly wasi wasic wasmer",
+    author="Syrus Akbary",
+    author_email="syrus@wasmer.io",
+    url="https://github.com/wasic-core/wasic",
     packages=['wasic'],
     include_package_data=True,
     license="MIT",
+    zip_safe=False,
     entry_points={'console_scripts': [
         'wasiar = wasic.wasiar:run',
         'wasic = wasic.wasic:run',
