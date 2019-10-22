454165e4a9d81f41f782214aa386305f4a5c0b3e
Syrus <me@syrusakbary.com>
2019-10-21 17:37:03 -0700

    Updated versino to 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author="Syrus Akbary",
     author_email="syrus@wasmer.io",
     url="https://github.com/wasienv-core/wasienv",
-    version='0.1.9',
+    version='0.2.0',
     packages=['wasienv'],
     include_package_data=True,
     license="MIT",
