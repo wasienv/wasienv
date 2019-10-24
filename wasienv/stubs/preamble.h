1c6ab0a98984e38f763ca88c2f30b958fb81cadf
Syrus <me@syrusakbary.com>
2019-10-24 15:39:49 -0700

    Improved preamble

--- wasienv/stubs/preamble.h
+++ wasienv/stubs/preamble.h
@@ -3,7 +3,9 @@
 #include <limits.h>
 #include <stdlib.h>
 
-// #define __wasilibc_unmodified_upstream 1
+#define __wasilibc_unmodified_upstream 1
+#include <stdlib.h>
+#undef __wasilibc_unmodified_upstream
 
 char *realpath(const char *path, char *resolved_path);
 uid_t getuid(void);
@@ -24,4 +26,17 @@ __attribute__((weak)) int mprotect(void *addr, size_t len, int prot) { return 0;
 __attribute__((weak)) int sysctlbyname(const char *name, void *oldp,	size_t *oldlenp, const void *newp, size_t newlen) { return 0; }
 
 __attribute__((weak)) pid_t getpid(void) { return 0; }
+
+__attribute__((weak)) uid_t geteuid(void) { return 0; }
+
+__attribute__((weak)) gid_t getgid(void) { return 0; }
+
+__attribute__((weak)) gid_t getegid(void) { return 0; }
+
+__attribute__((weak)) int chmod(const char *pathname, mode_t mode) { return 0; }
+
+__attribute__((weak)) int signal(int signum, int handler) { return 0; }
+
+__attribute__((weak)) int sigaction(int signum, const struct sigaction *act,
+                     struct sigaction *oldact) { return 0; }
 
