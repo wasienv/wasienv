be9c220fca12cf430b41756f41584279a26b45fb
Syrus <me@syrusakbary.com>
2019-11-13 14:44:53 -0800

    Updated version to 0.4.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author="Syrus Akbary",
     author_email="syrus@wasmer.io",
     url="https://github.com/wasienv-core/wasienv",
-    version='0.3.1',
+    version='0.4.0',
     packages=['wasienv'],
     include_package_data=True,
     license="MIT",
